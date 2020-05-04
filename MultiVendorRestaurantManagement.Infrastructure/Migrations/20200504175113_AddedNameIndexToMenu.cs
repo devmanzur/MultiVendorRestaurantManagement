1d4649f38a4bd70529e9d73fd7adc7b4dfcd9f66
levirgon <levirgon@gmail.com>
2020-05-04 23:57:55 +0600

    added menu index migration

---
+++ MultiVendorRestaurantManagement.Infrastructure/Migrations/20200504175113_AddedNameIndexToMenu.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MultiVendorRestaurantManagement.Infrastructure.Migrations
+{
+    public partial class AddedNameIndexToMenu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NameEng",
+                table: "Menu",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Menu",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Menu_Name",
+                table: "Menu",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Menu_NameEng",
+                table: "Menu",
+                column: "NameEng",
+                unique: true,
+                filter: "[NameEng] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Menu_Name",
+                table: "Menu");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Menu_NameEng",
+                table: "Menu");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NameEng",
+                table: "Menu",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Menu",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
